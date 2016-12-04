b79f733cf20571612a5dc3c69234e6b577365a5f
kizkoh <mac-daisuki@live.jp>
2016-12-05 04:34:41 +0900

    Change listening https to port 8000

--- webapp/react/server.jsx
+++ webapp/react/server.jsx
@@ -1,5 +1,5 @@
 import express from 'express';
-import https from 'https';
+import http from 'http';
 import fs from 'fs';
 import path from 'path';
 import React from 'react';
@@ -20,16 +20,16 @@ const apiBaseUrl = process.env.API;
 if (!apiBaseUrl) {
   throw 'Please set environment variable API=http://...';
 }
-if (!process.env.SSL_KEY) {
-  throw 'Please set environment variable SSL_KEY=/path/to/server.key';
-}
-if (!process.env.SSL_CERT) {
-  throw 'Please set environment variable SSL_CERT=/path/to/server.crt';
-}
+// if (!process.env.SSL_KEY) {
+//   throw 'Please set environment variable SSL_KEY=/path/to/server.key';
+// }
+// if (!process.env.SSL_CERT) {
+//   throw 'Please set environment variable SSL_CERT=/path/to/server.crt';
+// }
 
 const options = {
-  key: fs.readFileSync(process.env.SSL_KEY),
-  cert: fs.readFileSync(process.env.SSL_CERT),
+  // key: fs.readFileSync(process.env.SSL_KEY),
+  // cert: fs.readFileSync(process.env.SSL_CERT),
 };
 
 const app = express();
@@ -104,8 +104,9 @@ app.get('*', (req, res) => {
   });
 });
 
-const PORT = process.env.PORT || 443;
-https.createServer(options, app).listen(PORT);
+// const PORT = process.env.PORT || 443;
+const PORT = 8000;
+http.createServer(app).listen(PORT);
 
 function createHtml(appHtml, scriptTag, csrfToken) {
   return `<!DOCTYPE html>
