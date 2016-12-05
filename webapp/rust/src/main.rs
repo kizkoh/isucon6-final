9bf9b235135e581733c35dff29259f2e9ad9226a
kizkoh <mac-daisuki@live.jp>
2016-12-05 12:49:39 +0900

    Intermediate commit for rust

--- webapp/rust/src/main.rs
+++ webapp/rust/src/main.rs
@@ -1,4 +1,15 @@
-#![feature(custom_derive)]
+// How to run
+// ISUCON_ENV=prodution MYSQL_HOST=localhost MYSQL_PORT=3306 MYSQL_USER=isucon MYSQL_PASS=isucon cargo run
+
+// ISUCON_ENV: production // not need(?)
+// MYSQL_HOST: mysql
+// MYSQL_PORT: 3306
+// MYSQL_USER: isucon
+// MYSQL_PASS: isucon
+
+// mysql -uisucon -pisucon -h localhost
+
+#![feature(proc_macro)]
 #![feature(custom_attribute)]
 
 #[macro_use]
@@ -10,6 +21,8 @@ extern crate mysql;
 extern crate chrono;
 extern crate hyper;
 extern crate serde;
+#[macro_use]
+extern crate serde_derive;
 extern crate serde_json;
 
 // {Middleware, Action, StaticFilesHandler, FormBody};
@@ -32,35 +45,161 @@ use std::error::Error;
 use std::io::Write;
 use std::mem;
 use std::process::exit;
-
 use std::str::FromStr;
 use std::sync::Arc;
 
-#[allow(unused_attributes)]
 // #[derive(Debug, Serialize, Deserialize)]
-struct Token {
-    #[serde(rename="ID")]
+// struct Token {
+//     #[serde(rename="ID")]
+//     id: i64,
+//     #[serde(rename="CSRFToken")]
+//     csrf_token: String,
+//     #[serde(rename="CreatedAt")]
+//     created_at: NaiveDateTime,
+// }
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Point {
     id: i64,
-    #[serde(rename="CSRFToken")]
-    csrf_token: String,
-    #[serde(rename="CreatedAt")]
-    created_at: NaiveDateTime,
+    stroke_id: i64,
+    x: f64,
+    y: f64,
 }
+
+// type Point struct {
+// 	ID       int64   `json:"id" db:"id"`
+// 	StrokeID int64   `json:"stroke_id" db:"stroke_id"`
+// 	X        float64 `json:"x" db:"x"`
+// 	Y        float64 `json:"y" db:"y"`
+// }
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Stroke {
+    id: i64,
+    room_id: i64,
+    width: i64,
+    red: i64,
+    green: i64,
+    blue: i64,
+    alpha: f64,
+    // created_at: NaiveDateTime,
+    created_at: String,
+    points: Vec<Point>,
+}
+// type Stroke struct {
+// 	ID        int64     `json:"id" db:"id"`
+// 	RoomID    int64     `json:"room_id" db:"room_id"`
+// 	Width     int       `json:"width" db:"width"`
+// 	Red       int       `json:"red" db:"red"`
+// 	Green     int       `json:"green" db:"green"`
+// 	Blue      int       `json:"blue" db:"blue"`
+// 	Alpha     float64   `json:"alpha" db:"alpha"`
+// 	CreatedAt time.Time `json:"created_at" db:"created_at"`
+// 	Points    []Point   `json:"points" db:"points"`
+// }
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Room {
+    id: i64,
+    name: String,
+    canvas_width: i64,
+    canvas_height: i64,
+    // created_at: Vec<NaiveDateTime>,
+    created_at: String,
+    strokes: Vec<Stroke>,
+    stroke_count: i64,
+    watcher_count: i64,
+}
+
+// type Room struct {
+// 	ID           int64     `json:"id" db:"id"`
+// 	Name         string    `json:"name" db:"name"`
+// 	CanvasWidth  int       `json:"canvas_width" db:"canvas_width"`
+// 	CanvasHeight int       `json:"canvas_height" db:"canvas_height"`
+// 	CreatedAt    time.Time `json:"created_at" db:"created_at"`
+// 	Strokes      []Stroke  `json:"strokes"`
+// 	StrokeCount  int       `json:"stroke_count"`
+// 	WatcherCount int       `json:"watcher_count"`
+// }
 
 lazy_static! {
     pub static ref DBX: Arc<mysql::Pool> = Arc::new(conn());
 }
 
-fn generate_error<'mw, T: AsRef<str> + ?Sized>(mut res: Response<'mw>, msg: &T) -> MiddlewareResult<'mw> {
+fn get_strokes(room_id: i64, greather_than_id: i64) -> Result<Vec<Stroke>, mysql::Error> {
+    let mut query = "SELECT `id`, `room_id`, `width`, `red`, `green`, `blue`, `alpha`, `created_at` FROM `strokes`"
+        .to_string();
+    query += " WHERE `room_id` = :room_id AND `id` > :greather_than_id ORDER BY `id` ASC";
+    let mut strokes: Vec<Stroke> = vec![];
+    match DBX.prep_exec(query.clone(),
+                        (params!{"room_id" => room_id,
+                                 "greather_than_id" => greather_than_id,
+                        })) {
+        Ok(rows) => {
+            for row in rows {
+                // FixMe: Use mysql::from_row()
+                let mut row = row.unwrap();
+                let created_at: NaiveDateTime = row.take("created_at").unwrap();
+                let created_at = format!("{}", created_at.format("%Y-%m-%dT%H:%M:%S%.f%"));
+                let stroke = Stroke {
+                    id: row.take("id").unwrap(),
+                    room_id: row.take("room_id").unwrap(),
+                    width: row.take("width").unwrap(),
+                    red: row.take("red").unwrap(),
+                    green: row.take("green").unwrap(),
+                    blue: row.take("blue").unwrap(),
+                    alpha: row.take("alpha").unwrap(),
+                    created_at: created_at,
+                    points: vec![],
+                };
+                strokes.push(stroke);
+            }
+            Ok(strokes)
+        }
+        Err(e) => Err(e),
+    }
+}
+
+fn get_room(room_id: i64) -> Result<Option<Room>, mysql::Error> {
+    let query = "SELECT `id`, `name`, `canvas_width`, `canvas_height`, `created_at` FROM `rooms` WHERE `id` = :id";
+    match DBX.first_exec(query.clone(), (params!{"id" => room_id})) {
+        Ok(option) => {
+            match option {
+                Some(mut row) => {
+                    let created_at: NaiveDateTime = row.take("created_at").unwrap();
+                    let created_at = format!("{}", created_at.format("%Y-%m-%dT%H:%M:%S%.f%"));
+                    let room = Room {
+                        id: row.take("id").unwrap(),
+                        name: row.take("name").unwrap(),
+                        canvas_width: row.take("canvas_width").unwrap(),
+                        canvas_height: row.take("canvas_height").unwrap(),
+                        created_at: created_at,
+                        strokes: vec![],
+                        stroke_count: 0,
+                        watcher_count: 0,
+                    };
+                    return Ok(Some(room));
+                }
+                None => return Ok(None),
+            }
+        }
+        Err(e) => return Err(e),
+    }
+}
+
+// fn get_watcher_count(room_id: i64) -> Result<i64, Error> {
+// }
+
+fn output_error_msg<'mw, T: AsRef<str> + ?Sized>(mut res: Response<'mw>,
+                                                 status_code: StatusCode,
+                                                 msg: &T)
+    -> MiddlewareResult<'mw> {
     let err_msg: &str = unsafe { mem::transmute(msg.as_ref()) };
 
     // Implementation of Err struct
-    #[allow(unused_attributes)]
-    #[derive(Debug)]
+    #[derive(Debug, Deserialize)]
     struct Err {
-        // Error: String,
-        #[serde(rename="Error")]
-        error: &'static str,
+        error: String,
     };
     impl serde::Serialize for Err {
         fn serialize<S>(&self, serializer: &mut S) -> Result<(), S::Error>
@@ -71,8 +210,13 @@ fn generate_error<'mw, T: AsRef<str> + ?Sized>(mut res: Response<'mw>, msg: &T) 
             serializer.serialize_struct_end(err)
         }
     }
+    #[derive(Serialize, Deserialize)]
+    enum Errize {
+        #[serde(rename="error")]
+        Err(String),
+    }
 
-    let err = Err { error: err_msg };
+    let err = Errize::Err(err_msg.to_string());
     let b = match serde_json::to_string(&err) {
         Ok(b) => b,
         Err(e) => {
@@ -82,9 +226,45 @@ fn generate_error<'mw, T: AsRef<str> + ?Sized>(mut res: Response<'mw>, msg: &T) 
         }
     };
 
-    // ToDo: delete the following comments
+    // ToDo: Convert the following comments to rustdoc
     // let content_type: mime::Mime = "application/json;charset=utf-8".parse().unwrap();
     // res.set(ContentType(content_type.into()));
+    res.set(ContentType(Mime(TopLevel::Application,
+                             SubLevel::Json,
+                             vec![(Attr::Charset, Value::Utf8)])));
+    res.error(status_code, b)
+}
+
+fn output_error<'mw, T: AsRef<str> + ?Sized>(mut res: Response<'mw>, msg: &T) -> MiddlewareResult<'mw> {
+    // Implementation of Err struct
+    #[derive(Debug, Deserialize)]
+    struct Err {
+        error: String,
+    };
+    impl serde::Serialize for Err {
+        fn serialize<S>(&self, serializer: &mut S) -> Result<(), S::Error>
+            where S: serde::Serializer
+        {
+            let mut err = try!(serializer.serialize_struct("Err", 1));
+            try!(serializer.serialize_struct_elt(&mut err, "err", &self.error));
+            serializer.serialize_struct_end(err)
+        }
+    }
+    #[derive(Serialize, Deserialize)]
+    enum Errize {
+        #[serde(rename="error")]
+        Err(String),
+    }
+
+    let err = Errize::Err("InternalServerError".to_string());
+    let b = match serde_json::to_string(&err) {
+        Ok(b) => b,
+        Err(e) => {
+            let _result = writeln!(&mut std::io::stderr(), "{}", msg.as_ref());
+            let _result = writeln!(&mut std::io::stderr(), "{}", e.description());
+            exit(1);
+        }
+    };
     res.set(ContentType(Mime(TopLevel::Application,
                              SubLevel::Json,
                              vec![(Attr::Charset, Value::Utf8)])));
@@ -92,21 +272,13 @@ fn generate_error<'mw, T: AsRef<str> + ?Sized>(mut res: Response<'mw>, msg: &T) 
     res.error(StatusCode::InternalServerError, b)
 }
 
-// func outputError(w http.ResponseWriter, err error) {
-
-// 	b, _ := json.Marshal(struct {
-// 		Error string `json:"error"`
-// 	}{Error: "InternalServerError"})
-// 	fmt.Fprintln(os.Stderr, err.Error())
-// }
-
 fn post_api_csrf_token<'mw>(_req: &mut Request, mut res: Response<'mw>) -> MiddlewareResult<'mw> {
     let mut query = "INSERT INTO `tokens` (`csrf_token`) VALUES".to_string();
     query = query + &"(SHA2(CONCAT(RAND(), UUID_SHORT()), 256))".to_string();
 
     let last_insert_id = match DBX.prep_exec(query.clone(), ()) {
         Ok(row) => row.last_insert_id(),
-        Err(e) => return generate_error(res, e.description()),
+        Err(e) => return output_error(res, e.description()),
     };
 
     query = "SELECT `id`, `csrf_token`, `created_at` FROM `tokens` WHERE id = :id".to_string();
@@ -117,10 +289,8 @@ fn post_api_csrf_token<'mw>(_req: &mut Request, mut res: Response<'mw>) -> Middl
                     let csrf_token: String = row.take("csrf_token").unwrap();
 
                     // Implementation of Token struct
-                    #[allow(unused_attributes)]
-                    #[derive(Debug)]
+                    #[derive(Debug, Deserialize)]
                     struct Token {
-                        #[serde(rename="Token")]
                         token: String,
                     };
                     impl serde::Serialize for Token {
@@ -132,11 +302,16 @@ fn post_api_csrf_token<'mw>(_req: &mut Request, mut res: Response<'mw>) -> Middl
                             serializer.serialize_struct_end(token)
                         }
                     }
+                    #[derive(Serialize, Deserialize)]
+                    enum Tokenize {
+                        Token(String),
+                    }
 
-                    let token = Token { token: csrf_token };
+                    // Use enum Tokenize::Token();
+                    let token = Tokenize::Token(csrf_token);
                     let b = match serde_json::to_string(&token) {
                         Ok(b) => b,
-                        Err(e) => return generate_error(res, e.description()),
+                        Err(e) => return output_error(res, e.description()),
                     };
                     res.set(ContentType(Mime(TopLevel::Application,
                                              SubLevel::Json,
@@ -144,20 +319,74 @@ fn post_api_csrf_token<'mw>(_req: &mut Request, mut res: Response<'mw>) -> Middl
                     res.set(StatusCode::Ok);
                     return res.send(b);
                 }
-                None => return generate_error(res, "row is none"),
+                None => return output_error(res, "row is none"),
             };
         }
-        Err(e) => return generate_error(res, e.description()),
-    };
-
-    // b, _ := json.Marshal(struct {
-    // 	Token string `json:"token"`
-    // }{Token: t.CSRFToken})
+        Err(e) => return output_error(res, e.description()),
+    }
 }
 
 fn get_api_rooms<'mw>(_req: &mut Request, mut res: Response<'mw>) -> MiddlewareResult<'mw> {
-    res.set(StatusCode::Ok);
-    res.send("ok")
+    let mut query = "SELECT `room_id`, MAX(`id`) AS `max_id` FROM `strokes`".to_string();
+    query += " GROUP BY `room_id` ORDER BY `max_id` DESC LIMIT 100";
+
+    let mut rooms: Vec<Room> = vec![];
+
+    match DBX.prep_exec(query.clone(), ()) {
+        Ok(rows) => {
+            for row in rows {
+                let mut row = row.unwrap();
+                let room_id: i64 = row.take("room_id").unwrap();
+                let mut room = match get_room(room_id) {
+                    Ok(room) => {
+                        match room {
+                            Some(room) => room,
+                            None => return output_error(res, "row is none"),
+                        }
+                    }
+                    Err(e) => return output_error(res, e.description()),
+                };
+
+                let s = match get_strokes(room.id, 0) {
+                    Ok(s) => s,
+                    Err(e) => return output_error(res, e.description()),
+                };
+                room.stroke_count = s.len() as i64;
+                rooms.push(room);
+            }
+            // Implementation of Token struct
+            #[derive(Debug, Deserialize)]
+            struct RoomResp {
+                rooms: Vec<Room>,
+            };
+            impl serde::Serialize for RoomResp {
+                fn serialize<S>(&self, serializer: &mut S) -> Result<(), S::Error>
+                    where S: serde::Serializer
+                {
+                    let mut room_resp = try!(serializer.serialize_struct("room_resp", 1));
+                    try!(serializer.serialize_struct_elt(&mut room_resp, "rooms", &self.rooms));
+                    serializer.serialize_struct_end(room_resp)
+                }
+            }
+            #[derive(Serialize, Deserialize)]
+            enum RoomRespize {
+                RoomResp(Vec<Room>),
+            }
+
+            let room_resp = RoomRespize::RoomResp(rooms);
+            let b = match serde_json::to_string(&room_resp) {
+                Ok(b) => b,
+                Err(e) => return output_error(res, e.description()),
+            };
+
+            res.set(ContentType(Mime(TopLevel::Application,
+                                     SubLevel::Json,
+                                     vec![(Attr::Charset, Value::Utf8)])));
+            res.set(StatusCode::Ok);
+            return res.send(b);
+        }
+        Err(e) => return output_error(res, e.description()),
+    }
 }
 
 fn post_api_rooms<'mw>(_req: &mut Request, mut res: Response<'mw>) -> MiddlewareResult<'mw> {
@@ -204,7 +433,7 @@ fn conn() -> mysql::Pool {
         .tcp_port(port)
         .db_name(Some(dbname))
         .init(vec!["charset=utf8mb4", "parseTime=true", "loc=Local"]); // 要検証
-    // Use mysql::conn::new?
+    // FixMe: Use mysql::conn::new?
     match mysql::Pool::new(opts) {
         Ok(dbx) => dbx,
         Err(e) => {
